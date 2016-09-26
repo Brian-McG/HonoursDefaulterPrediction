dfac62cba7594ebde49174aa1f59f2f0806e828d
Brian Mc George <Brian-McG.github@outlook.com>
2016-09-26 17:28:23 +0200

    Improve use of processor cores

--- src/util.py
+++ src/util.py
@@ -9,9 +9,5 @@ def verbose_print(message=""):
 
 
 def get_number_of_processes_to_use():
-    logical_cpu_count = psutil.cpu_count(logical=True)
     physical_cpu_count = psutil.cpu_count(logical=False)
-    if logical_cpu_count == physical_cpu_count:
-        return physical_cpu_count + physical_cpu_count / 2
-    else:
-        return logical_cpu_count
+    return physical_cpu_count * 2
