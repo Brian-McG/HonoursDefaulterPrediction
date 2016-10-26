64c2ea2769f7ed952db229159da71f2d10836414
Brian Mc George <Brian-McG.github@outlook.com>
2016-10-27 00:45:38 +0200

    Update dataset config

--- src/config/data_sets.py
+++ src/config/data_sets.py
@@ -1,19 +1,23 @@
 """Data sets to test against"""
 import os
 
-from config import lima_tb_classifier_parameters, default_classifier_parameters, german_classifier_parameters, australian_classifier_parameters
+from config import australian_data_balancer_only_parameters
+from config import german_data_balancer_only_parameters
+from config import lima_tb_classifier_parameters,  default_classifier_parameters, german_classifier_parameters, australian_classifier_parameters
+from config import lima_tb_data_balancer_only_parameters
+from config.constants import LOGISTIC_REGRESSION, BERNOULLI_NAIVE_BAYES, SVM_LINEAR, DECISION_TREE, RANDOM_FOREST, ANOVA_CHI2
 
 data_set_arr = []
 
 
 def append_data_set_details(data_set_path, numeric_columns, categorical_columns, classification_label, time_to_default, missing_values_strategy, feature_selection_strategy, status, classifier_parameters,
-                            data_set_description,
+                            data_set_description, data_set_data_balancer_parameters, duplicate_removal_column,
                             data_sets):
     """Adds data set path, data set status and data set description to data_set_arr as a dictionary"""
     data_sets.append({"data_set_path": data_set_path, "numeric_columns": numeric_columns, "categorical_columns": categorical_columns, "classification_label": classification_label,
                       "time_to_default": time_to_default, "missing_values_strategy": missing_values_strategy,
                       "feature_selection_strategy": feature_selection_strategy, "status": status, "data_set_classifier_parameters": classifier_parameters,
-                      "data_set_description": data_set_description})
+                      "data_set_description": data_set_description, "data_set_data_balancer_parameters": data_set_data_balancer_parameters, "duplicate_removal_column": duplicate_removal_column})
 
 
 # Lima TB data set
@@ -24,12 +28,11 @@ lima_tb_categorical_columns = ["Age", "Sex", "Marital Status", "Prison Hx", "Com
 lima_tb_classification_label = ["Treatment Outcome"]
 lima_tb_time_to_default = "Time to Default (Days)"
 lima_tb_missing_values_strategy = "remove"
-lima_tb_feature_selection_strategy = "Anova"
+lima_tb_feature_selection_strategy = None
 lima_tb_enabled = False
-lima_tb_classifier_parameters = lima_tb_classifier_parameters
 append_data_set_details(lima_tb_path, lima_tb_numeric_columns, lima_tb_categorical_columns, lima_tb_classification_label, lima_tb_time_to_default, lima_tb_missing_values_strategy,
                         lima_tb_feature_selection_strategy, lima_tb_enabled,
-                        lima_tb_classifier_parameters, "Lima_TB", data_set_arr)
+                        lima_tb_classifier_parameters, "Lima_TB", lima_tb_data_balancer_only_parameters, None, data_set_arr)
 
 # # Baobab data set
 # baobab_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "<TODO>")
@@ -45,11 +48,11 @@ german_credit_categorical_columns = ["Attribute 1", "Attribute 3", "Attribute 4"
 german_credit_classification_label = ["Classification Label"]
 german_credit_time_to_default = None
 german_credit_missing_values_strategy = "remove"
-german_credit_feature_selection_strategy = "Anova"
+german_credit_feature_selection_strategy = None
 german_credit_enabled = False
 german_credit_parameters = german_classifier_parameters
 append_data_set_details(german_credit_path, german_credit_numeric_columns, german_credit_categorical_columns, german_credit_classification_label, german_credit_time_to_default,
-                        german_credit_missing_values_strategy, german_credit_feature_selection_strategy, german_credit_enabled, german_credit_parameters, "German_credit", data_set_arr)
+                        german_credit_missing_values_strategy, german_credit_feature_selection_strategy, german_credit_enabled, german_credit_parameters, "German_credit", german_data_balancer_only_parameters, None, data_set_arr)
 
 # Australian credit data set
 australian_credit_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/../../data/australian_finance/australian.csv")
@@ -58,31 +61,28 @@ australian_credit_categorical_columns = ["A1", "A4", "A5", "A6", "A8", "A9", "A1
 australian_credit_classification_label = ["Classification Label"]
 australian_credit_time_to_default = None
 australian_credit_missing_values_strategy = "remove"
-australian_credit_feature_selection_strategy = "Anova"
-australian_credit_enabled = True
+australian_credit_feature_selection_strategy = None
+australian_credit_enabled = False
 australian_credit_parameters = australian_classifier_parameters
 append_data_set_details(australian_credit_path, australian_credit_numeric_columns, australian_credit_categorical_columns, australian_credit_classification_label,
                         australian_credit_time_to_default,
-                        australian_credit_missing_values_strategy, australian_credit_feature_selection_strategy, australian_credit_enabled, australian_credit_parameters, "Australian_credit", data_set_arr)
+                        australian_credit_missing_values_strategy, australian_credit_feature_selection_strategy, australian_credit_enabled, australian_credit_parameters, "Australian_credit", australian_data_balancer_only_parameters, None, data_set_arr)
 
-# # Lima TB data set
-# lima_tb_reduced_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/../../data/lima_tb/Lima TB Treatment Default Data.csv")
-# lima_tb_reduced_numeric_columns = []
-# # lima_tb_reduced_categorical_columns = ["Completed Secondary Education", "Alcohol Use at Least Once Per Week", "History of Drug Use", "MDR-TB", "Body Mass Index", "HIV Status"]
-# # lima_tb_reduced_categorical_columns = ["Completed Secondary Education", "Alcohol Use at Least Once Per Week", "History of Drug Use", "MDR-TB", "Hx Diabetes Melitus"]
-# lima_tb_reduced_categorical_columns = ["Sex", "Prison Hx", "Completed Secondary Education", "Hx of Tobacco Use", "Alcohol Use at Least Once Per Week",
-#                                        "History of Drug Use", "Hx of Rehab", "MDR-TB", "Body Mass Index", "HIV Status", "Hx Diabetes Melitus"]
-# lima_tb_reduced_classification_label = ["Treatment Outcome"]
-# lima_tb_reduced_time_to_default = None
-# lima_tb_reduced_missing_values_strategy = "remove"
-# lima_tb_reduced_enabled = False
-# lima_tb_reduced_classifier_parameters = default_classifier_parameters
-# append_data_set_details(lima_tb_reduced_path, lima_tb_reduced_numeric_columns, lima_tb_reduced_categorical_columns, lima_tb_reduced_classification_label,
-#                         lima_tb_reduced_time_to_default,
-#                         lima_tb_reduced_missing_values_strategy, lima_tb_reduced_enabled,
-#                         lima_tb_reduced_classifier_parameters, "Lima_TB reduced parameters", data_set_arr)
-#
-#
+# CHW Attrition
+chw_attrition_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/../../data/chw_attrition/chw_data_india.csv")
+chw_attrition_numeric_columns = ["X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12", "X13", "X14", "X15", "X16", "X17", "X18", "X19", "X20", "X21", "X22", "X23", "X24", "X25", "X26", "X27", "X28", "X29", "X30", "X31", "X32", "X33", "X34", "X35", "X36", "X37", "X38", "X39", "X40", "X41", "X42", "X43", "X44", "X45", "X46", "X47", "X48", "X49", "X50", "X51", "X52", "X53", "X54", "X55", "X56", "X57", "X58", "X59", "X60", "X61", "X62", "X63", "X64", "X65", "X66", "X67", "X68", "X69", "X70", "X71", "X72", "X73", "X74", "X75", "X76", "X77", "X78", "X79", "X80", "X81", "X82", "X83", "X84", "X85", "X86", "X87", "X88", "X89", "X90"]
+chw_attrition_categorical_columns = ["projectCode", "sector"]
+chw_attrition_classification_label = ["attritted"]
+chw_attrition_time_to_default = None
+chw_attrition_missing_values_strategy = "remove"
+chw_attrition_feature_selection_strategy = None
+chw_attrition_enabled = True
+chw_attrition_parameters = australian_classifier_parameters
+append_data_set_details(chw_attrition_path, chw_attrition_numeric_columns, chw_attrition_categorical_columns, chw_attrition_classification_label,
+                        chw_attrition_time_to_default,
+                        chw_attrition_missing_values_strategy, chw_attrition_feature_selection_strategy, chw_attrition_enabled, lima_tb_classifier_parameters, "India_CHW_attrition", lima_tb_data_balancer_only_parameters, "userCode", data_set_arr)
+
+
 # # HIV
 # hiv_path = os.path.abspath(os.path.dirname(os.path.realpath(__file__)) + "/../../data/STEPS data PLOS ONE.csv")
 # hiv_numeric_columns = ["CD4 cell ct", "Age"]
